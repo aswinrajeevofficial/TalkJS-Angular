20f400408756abdc281da0f8cd687bfeb068615b
aswinrajeevofficial <aswinworkofficial@gmail.com>
2021-09-25 12:14:26 +0100

    initial commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDN-ekTCi9P9pU0ofgH3BBs1T25RnVwIio",
+    authDomain: "talkjs-angular.firebaseapp.com",
+    projectId: "talkjs-angular",
+    storageBucket: "talkjs-angular.appspot.com",
+    messagingSenderId: "1095757418951",
+    appId: "1:1095757418951:web:1081abbe11ae6777c1c9e2"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
